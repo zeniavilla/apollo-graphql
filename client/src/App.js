623132eba37201a4cb533cc832e1246c62c7aee3
Zenia <zeniavilla@hotmail.com>
2017-10-28 12:56:16 -0700

    Add ChannelsList component

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Components
+import ChannelsList from './ChannelsList';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Apollo</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ChannelsList />
       </div>
     );
   }
---
+++ client/src/ChannelsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChannelsList = () =>
+  <ul>
+    <li>Channel 1</li>
+    <li>Channel 2</li>
+  </ul>
+
+export default ChannelsList;
